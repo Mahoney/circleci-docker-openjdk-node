4e2df7b10fcef5cc868ccc522c127dad23ee6168
Robert Elliot <rob@lidalia.org.uk>
2017-06-26 16:55:55 +0100

    Stop trying to specify a docker registry when sending to docker hub

--- bash_libs/deploy.sh
+++ bash_libs/deploy.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
-set -ueo pipefail
+set -uexo pipefail
 IFS=$'\n\t'
 
 source $(dirname "$0")/get_version.sh
 
 new_version=$(get_version)
+
+docker_registry=${DOCKER_REGISTRY:-""}
+
+if [ -z "$docker_registry" ]; then
+  docker_image="$DOCKER_REPO/$DOCKER_ARTIFACT"
+else
+  docker_image="$docker_registry/$DOCKER_REPO/$DOCKER_ARTIFACT"
+fi
 
 docker_image="$DOCKER_REPO/$DOCKER_ARTIFACT"
 docker_image_latest="$docker_image:latest"
@@ -14,7 +22,7 @@ docker_image_versioned="$docker_image:$new_version"
 git tag -a "$new_version" -m "Release version $new_version"
 
 docker tag "$docker_image_latest" "$docker_image_versioned"
-docker login -u "$DOCKER_ID" -p "$DOCKER_PASSWORD" "${DOCKER_REGISTRY:-registry.hub.docker.com}"
+docker login -u "$DOCKER_ID" -p "$DOCKER_PASSWORD" "$docker_registry"
 docker push "$docker_image_latest"
 docker push "$docker_image_versioned"
 git push origin "$new_version"
