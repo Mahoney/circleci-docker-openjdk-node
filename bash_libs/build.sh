246c2d986a1c19168bdcc0f552d6891f7b3e3965
Robert Elliot <rob@lidalia.org.uk>
2017-06-30 10:32:59 +0100

    Clean up some names and remove an unused script

--- bash_libs/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ueo pipefail
-IFS=$'\n\t'
-
-source $(dirname "$0")/docker.sh
-docker_image=$1
-
-docker build -t "$docker_image:latest" .
--- bash_libs/deploy-to-repo.sh
+++ bash_libs/deploy-to-repo.sh
@@ -5,12 +5,14 @@ IFS=$'\n\t'
 
 source $(dirname "$0")/docker.sh
 
-docker_image_latest=$1
+docker_image_to_deploy=$1
 new_version=$2
 
 docker_image=$(calc_docker_image)
 docker_image_versioned="$docker_image:$new_version"
 
-docker tag "$docker_image_latest" "$docker_image_versioned"
-docker login -u "$DOCKER_ID" -p "$DOCKER_PASSWORD" ${DOCKER_REGISTRY:-""}
-docker push "$docker_image_versioned"
+docker tag "$docker_image_to_deploy" "$docker_image_versioned" > /dev/null
+docker login -u "$DOCKER_ID" -p "$DOCKER_PASSWORD" ${DOCKER_REGISTRY:-""} > /dev/null
+docker push "$docker_image_versioned" > /dev/null
+echo "$docker_image_versioned"
+
