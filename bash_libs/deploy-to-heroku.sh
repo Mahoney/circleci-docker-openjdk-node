ab031f488d3049767e223c36de3e97030bc9cd62
Robert Elliot <rob@lidalia.org.uk>
2017-07-06 23:21:54 +0100

    Allow creating apps for a team

--- bash_libs/deploy-to-heroku.sh
+++ bash_libs/deploy-to-heroku.sh
@@ -3,10 +3,11 @@
 set -ueo pipefail
 IFS=$'\n\t'
 
-docker_image_versioned=$1
-app_env=$2
-base_heroku_project=$3
-base_domain_name=$4
+docker_image_versioned=${1:?"Pass the full docker image tag to be deployed as arg 1"}
+app_env=${2:?"Pass the env to deploy to as arg 2"}
+base_heroku_project=${3:?"Pass the base heroku project as arg 3"}
+base_domain_name=${4:?"Pass the base domain name as arg 4"}
+team_name=${5:?"Pass the heroku team name as arg 5"}
 
 heroku_project="${app_env}-${base_heroku_project}"
 new_env=false
@@ -14,7 +15,7 @@ new_env=false
 : ${HEROKU_API_KEY:?"Need to set HEROKU_API_KEY env var to interact with heroku"}
 
 if ! heroku apps:info "$heroku_project" > /dev/null 2>&1; then
-  heroku apps:create "$heroku_project" --no-remote --region eu
+  heroku apps:create "$heroku_project" --no-remote --region eu --team "$team_name"
   new_env=true
 fi
 
