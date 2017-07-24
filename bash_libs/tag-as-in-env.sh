a6cfb5df0bb18aea86294e21d894195a01d2240a
Robert Elliot <rob@lidalia.org.uk>
2017-07-24 22:54:12 +0100

    Use an ISO 8601 standard timestamp including showing it is UTC

--- bash_libs/tag-as-in-env.sh
+++ bash_libs/tag-as-in-env.sh
@@ -8,5 +8,5 @@ app_env=$2
 
 git checkout $new_version
 git tag -a -f "running_in_${app_env}" -m ""
-git tag -a -f "deployed_to_${app_env}_$(date -u +"%Y-%m-%dT%H-%M")" -m "Deployed to ${app_env} by CD"
+git tag -a -f "deployed_to_${app_env}_$(date -u +"%Y-%m-%dT%H%M")Z" -m "Deployed to ${app_env} by CD"
 git push --tags
