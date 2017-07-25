23b5b40c35aa04b1ec887c042926f064e0a424a9
Robert Elliot <rob@lidalia.org.uk>
2017-07-25 15:25:29 +0100

    Instigate docker caching

--- bash_libs/cache-docker.sh
+++ bash_libs/cache-docker.sh
@@ -2,13 +2,14 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-image=$1
-
 docker_cache_dir=~/.dockercache
 
-if [ -f $docker_cache_dir/$image/image.tar ]; then
-  docker load -i $docker_cache_dir/$image/image.tar
-else
-  docker pull $image
-  mkdir -p $docker_cache_dir/$image; docker save $image > $docker_cache_dir/$image/image.tar
-fi
+for image in "$@"; do
+  if [ -f $docker_cache_dir/$image/image.tar ]; then
+    docker load -i $docker_cache_dir/$image/image.tar
+  else
+    docker pull $image
+    mkdir -p $docker_cache_dir/$image
+    docker save $image > $docker_cache_dir/$image/image.tar
+  fi
+done
