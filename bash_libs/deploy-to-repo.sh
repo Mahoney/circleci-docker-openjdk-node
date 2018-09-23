7efc78bd178f66bf00a2dbd6a000874950b91d03
Robert Elliot <rob@lidalia.org.uk>
2018-09-23 22:37:59 +0100

    Get errors next time deploy to repo fails

--- bash_libs/deploy-to-repo.sh
+++ bash_libs/deploy-to-repo.sh
@@ -11,7 +11,7 @@ new_version=$2
 docker_image=$(calc_docker_image)
 docker_image_versioned="$docker_image:$new_version"
 
-docker tag "$docker_image_to_deploy" "$docker_image_versioned" >/dev/null 2>&1
-docker login -u "$DOCKER_ID" -p "$DOCKER_PASSWORD" ${DOCKER_REGISTRY:-""} >/dev/null 2>&1
-docker push "$docker_image_versioned" >/dev/null 2>&1
+docker tag "$docker_image_to_deploy" "$docker_image_versioned"
+docker login -u "$DOCKER_ID" -p "$DOCKER_PASSWORD" ${DOCKER_REGISTRY:-""}
+docker push "$docker_image_versioned"
 echo -n "$docker_image_versioned"
